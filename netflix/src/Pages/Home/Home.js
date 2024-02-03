d2480e3db972fbf5574bbcc65068b3000832c703
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-03 01:12:28 -0500

    add axios module, create API constum request and .env

--- netflix/src/Pages/Home/Home.js
+++ netflix/src/Pages/Home/Home.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-
-// import RowList from "../../Componets/Rows/RowList/RowList"
-// import Banner from '../../Componets/Banner/Banner'
+import RowList from "../../Components/Rows/RowList/RowList"
+import Banner from '../../Components/Banner/Banner'
 
 function Home() {
   return (
     <>
     <Header/> 
-    {/* <Banner />
-    <RowList /> */}
+    <Banner /> 
+    <RowList />
     <Footer/>  
     </>
-   
+
   )
 }
 
