20dc695821f0687f6e5469796a03d2194e4089f3
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-03 00:36:49 -0500

    header and footer components

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,13 @@
+import Home from './Pages/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>React App</h1>
+      <Home/>
     </div>
   );
 }
 
 export default App;
+
---
+++ netflix/src/Pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+
+// import RowList from "../../Componets/Rows/RowList/RowList"
+// import Banner from '../../Componets/Banner/Banner'
+
+function Home() {
+  return (
+    <>
+    <Header/> 
+    {/* <Banner />
+    <RowList /> */}
+    <Footer/>  
+    </>
+   
+  )
+}
+
+export default Home
